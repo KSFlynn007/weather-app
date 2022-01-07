2254ab818ad6daedd4342b6835eae4b2ed0719b3
KSFlynn007 <kelseyflynnn@gmail.com>
2022-01-07 08:38:37 -0700

    removed redundant component
    was acting as another layer of an entry way point that could be combined in the root App.js component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import Forecast from "./components/Forecast";
-import MainView from "./components/MainView/MainView";
 import './index.css';
 
+import React from "react";
+import Search from "./components/Search/Search";
+import Card from "./components/Card/Card";
+import DetailCard from "./components/DetailCard/DetailCard";
+import PreviewCard from "./components/PreviewCard/PreviewCard";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <MainView></MainView> */}
-      <h1>React Weather</h1>
-      <Forecast />
-    </div>
-  );
+  if(true){
+    return(
+      <>
+        <Forecast />
+        <DetailCard></DetailCard>
+      </>
+    )
+  } else{
+    return(
+      <div>
+          <Search></Search>
+          <Card></Card>
+          <div className='future-weather'>
+              <PreviewCard></PreviewCard>
+              <PreviewCard></PreviewCard>
+              <PreviewCard></PreviewCard>
+              <PreviewCard></PreviewCard>
+              <PreviewCard></PreviewCard>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
