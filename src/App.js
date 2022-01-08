348c2550d8863e5d727bdf27073a26c3aaea9d7f
KSFlynn007 <kelseyflynnn@gmail.com>
2022-01-07 17:33:22 -0700

    cleaned up App.js to nav to Forecast first

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import Forecast from "./components/Forecast";
 import './index.css';
-
 import React from "react";
-import Card from "./components/Card/Card";
-import DetailCard from "./components/DetailCard/DetailCard";
-import PreviewCard from "./components/PreviewCard/PreviewCard";
+
 
 function App() {
-  if(true){
-    return(
-      <Forecast />
-    )
-  } else{
-    return(
-      <div>
-          <Card></Card>
-          <div className='future-weather'>
-              <PreviewCard></PreviewCard>
-              <PreviewCard></PreviewCard>
-              <PreviewCard></PreviewCard>
-              <PreviewCard></PreviewCard>
-              <PreviewCard></PreviewCard>
-          </div>
-      </div>
-    )
-  }
+  return(
+    <Forecast/>
+  )
 }
 
 export default App;
