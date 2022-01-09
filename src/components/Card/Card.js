4d6414276d911309b30fb4e5548cc50be60659ae
KSFlynn007 <kelseyflynnn@gmail.com>
2022-01-09 15:37:24 -0700

    combined Card with DetailCard component
    lots of overlap, no need for both

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import './card.css';
+import PreviewCard from "../PreviewCard/PreviewCard";
 
-export default function Card(){
+function convertTime(timestamp){
+    let unix_timestamp = timestamp
+    let date = new Date(unix_timestamp * 1000);
+    const time = date.toLocaleTimeString();
+
+    return time;
+}
+
+function convertDate(timestamp){
+    let unix_timestamp = timestamp
+    let convertedDate = new Date(unix_timestamp * 1000);
+
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const result = (convertedDate.toLocaleDateString('en-US', options));
+
+    return result;
+}
+
+function capitalize(text){
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+export default function Card(props){
+    const [submitted, setSubmitted] = useState(false);
+
+    let currentDate = convertDate(props.responseObj.dt);
+    let icon = props.weatherIcon;
+
+    function handleSubmit(){
+        if(!submitted){
+            setSubmitted(true)
+        } else {
+            setSubmitted(false);
+        }
+    }
 
     return(
-        <div className="card">
-            <p className="today-date">January 7th, 2022</p>
-            <h2>Calgary, AB</h2>
-            <img className="weather-icon" src="https://olc-wordpress-assets.s3.amazonaws.com/uploads/2020/05/cloud-icon-300x198.png" alt="weather-icon" />
-            <div className="weather-info">
-                <p><span className="info-title">Currently:</span> -10C</p>
-                <p><span className="info-title">Feels Like:</span> -15C</p>
-                <p><span className="info-title">Daily High:</span> -7C</p>
-                <p><span className="info-title">Daily Low: </span> -20C</p>
-            </div>
-            {/* need to pass a state back to the parent component to be checked */}
-            {/* onClick={() => setSubmitted(true)} */}
-            <button className="seeMore-btn">See More</button>
+        <div>
+            {props.error && <p className="error-message">Please enter a valid city.</p>}
+            {props.loading && <p className="loading-message">Loading...</p>}
+
+            {props.responseObj.cod === 200 ?
+                <div>
+                    <div className="card card-detail">
+                        <p className="today-date">{currentDate}</p>
+                        <h2>{props.responseObj.name}, {props.responseObj.sys.country}</h2>
+                        <img className="weather-icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather-icon" />
+                        <p className="icon-details">{capitalize(props.responseObj.weather[0].description)}</p>
+                        <div className="weather-info">
+                            <p><span className="info-title">Currently:</span> {Math.round(props.responseObj.main.temp)} &deg;C</p>
+                            <p><span className="info-title">Feels Like:</span> {Math.round(props.responseObj.main.feels_like)} &deg;C</p>
+                            <p><span className="info-title">Daily High:</span> {Math.round(props.responseObj.main.temp_max)} &deg;C </p>
+                            <p><span className="info-title">Daily Low: </span> {Math.round(props.responseObj.main.temp_min)} &deg;C </p>
+    
+                            {submitted ?
+                                <>
+                                    <p>
+                                        <img src={require("../../img/sunrise.png")} alt="sunrise-icon" className="sun-icon" />
+                                        <span className="info-title">Sunrise:</span> {convertTime(props.responseObj.sys.sunrise)}
+                                    </p>
+                                    <p>
+                                        <img src={require("../../img/sunset.png")} alt="sunset-icon" className="sun-icon" />
+                                        <span className="info-title">Sunset:</span> {convertTime(props.responseObj.sys.sunset)}
+                                    </p>
+                                </>
+                            : null
+                            }
+                        </div>
+                        <button className="seeMore-btn" onClick={() => handleSubmit()}>Go Back</button>
+                    </div>
+                    {!submitted ? 
+                        <div className='future-weather'>
+                            <PreviewCard></PreviewCard>
+                            <PreviewCard></PreviewCard>
+                            <PreviewCard></PreviewCard>
+                            <PreviewCard></PreviewCard>
+                            <PreviewCard></PreviewCard>
+                        </div>
+                    : null }
+                </div>    
+            : null
+            }
         </div>
     )
 }
