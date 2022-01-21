30659940e1c262c893b3a8569b380bbccdc1848b
KSFlynn007 <kelseyflynnn@gmail.com>
2022-01-20 17:27:27 -0700

    moved first load geolocation to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import Forecast from "./components/Forecast";
 import './index.css';
-import React from "react";
+import React, { useState } from "react";
 
 
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  if('geolocation' in navigator){
+      const options = {
+          enableHighAccuracy : false, 
+          timeout: 5000,
+          maximumAge: 0
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+
+      function success(pos){
+          let lng = pos.coords.longitude;
+          let lat = pos.coords.latitude;
+
+          setLatitude(lat);
+          setLongitude(lng);
+      }
+
+      function error(err){
+          console.log(err);
+      }
+  } else {
+      console.log('Sorry, looks like your browser doesn\'t support geolocation!');
+  }
+
   return(
-    <Forecast/>
+    <Forecast
+      lat={latitude}
+      lng={longitude}/>
   )
 }
 
